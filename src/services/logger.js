8b3e22888bf2e5c12c653332a642e2add35c88a6
Manuel Serra <nitroin@hotmail.com>
2017-10-31 11:11:28 +0100

    [chore]: update log name

--- src/services/logger.js
+++ src/services/logger.js
@@ -3,6 +3,6 @@ import bunyan from "bunyan";
 import { LOG_LEVEL } from "../config";
 
 export const log = bunyan.createLogger({
-    name: "cycle2work-auth",
+    name: "cycle2work-activities",
     level: (process.env.NODE_ENV === "test" && !process.env.LOG_LEVEL) ? "fatal" : LOG_LEVEL
 });
