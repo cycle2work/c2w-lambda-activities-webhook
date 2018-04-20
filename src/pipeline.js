6c6bc7879aeec45d1c64e1268256ac765be9a1b0
Manuel Serra <nitroin@hotmail.com>
2018-04-20 15:21:38 +0200

    [feat]: adds Strava webhooks subscription logic

--- src/pipeline.js
+++ src/pipeline.js
@@ -1,15 +1,23 @@
 import { map } from "bluebird";
+import axios from "axios";
 
 import { log } from "./services/logger";
 import { upsertActivity, retrieveAthlete } from "./services/mongo-db";
 import { getActivity } from "./services/strava";
 
+import { STRAVA_WEBHOOKS_URL } from "./config";
+
 export default async function pipeline(event, context) {
     try {
         log.debug({ event });
 
-        const { body } = event;
-        log.debug({ body });
+        const { body, httpMethod, queryStringParameters } = event;
+        log.debug({ body, httpMethod, queryStringParameters });
+
+        if (httpMethod === "GET") {
+            const response = await axios.get(`${STRAVA_WEBHOOKS_URL}?hub.challenge=${queryStringParameters["hub.challenge"]}`);
+            log.debug({ response });
+        }
 
         const parsed = JSON.parse(body);
         log.debug({ parsed });
