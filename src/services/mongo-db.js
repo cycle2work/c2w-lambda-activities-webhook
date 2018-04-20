644214581ed39a77950d415fb25720022849e74f
Manuel Serra <nitroin@hotmail.com>
2018-04-21 00:16:56 +0200

    [chore]: remove unused code 💀

--- src/services/mongo-db.js
+++ src/services/mongo-db.js
@@ -1,12 +1,6 @@
 import { MongoClient } from "mongodb";
 
-import {
-    MONGODB_URL,
-    ACTIVITIES_COLLECTION,
-    PROCESSED_ACTIVITIES_COLLECTION,
-    ATHLETES_COLLECTION,
-    CLUBS_COLLECTION
-} from "../config";
+import { MONGODB_URL, ACTIVITIES_COLLECTION, ATHLETES_COLLECTION } from "../config";
 
 let dbInstance;
 
@@ -15,30 +9,6 @@ export async function getMongoClient() {
         dbInstance = await MongoClient.connect(MONGODB_URL);
     }
     return dbInstance;
-}
-
-export async function retrieveClubs(query = {}) {
-    const db = await getMongoClient();
-    return await db
-        .collection(CLUBS_COLLECTION)
-        .find(query)
-        .toArray();
-}
-
-export async function retrieveActivities(query = {}) {
-    const db = await getMongoClient();
-    return await db
-        .collection(ACTIVITIES_COLLECTION)
-        .find(query)
-        .toArray();
-}
-
-export async function retrieveProcessedActivities(query = {}) {
-    const db = await getMongoClient();
-    return await db
-        .collection(PROCESSED_ACTIVITIES_COLLECTION)
-        .find(query)
-        .toArray();
 }
 
 export async function retrieveAthlete(athleteId) {
--- src/services/strava.js
+++ src/services/strava.js
@@ -2,6 +2,4 @@ import strava from "strava-v3";
 
 import { promisify } from "bluebird";
 
-export const listAthleteClubs = promisify(strava.athlete.listClubs);
-export const listClubActivities = promisify(strava.clubs.listActivities);
 export const getActivity = promisify(strava.activities.get);
