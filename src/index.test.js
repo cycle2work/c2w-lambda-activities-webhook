986415df47fd8a0bec4312ca7be5b6dfc83b14b6
Manuel Serra <nitroin@hotmail.com>
2020-07-16 12:29:35 +0200

    chore: avoid mongo db client leak

--- src/index.test.js
+++ src/index.test.js
@@ -1,5 +1,5 @@
 import { handler } from ".";
-import { ACTIVITIES_COLLECTION, ATHLETES_COLLECTION } from "./config";
+import { ACTIVITIES_COLLECTION, ATHLETES_COLLECTION, DB_NAME } from "./config";
 
 import { mockedActivity, mockedRefreshTokenResponse } from "./mocks/strava";
 import * as strava from "./services/strava";
@@ -9,11 +9,11 @@ import { Promise } from "bluebird";
 import moment from "moment";
 
 strava.getActivity = jest.fn(() => {
-    return new Promise(resolve => resolve(mockedActivity));
+    return new Promise((resolve) => resolve(mockedActivity));
 });
 
 strava.refreshToken = jest.fn(() => {
-    return new Promise(resolve => resolve(mockedRefreshTokenResponse));
+    return new Promise((resolve) => resolve(mockedRefreshTokenResponse));
 });
 
 describe("Cycle2work activities function", () => {
@@ -21,38 +21,40 @@ describe("Cycle2work activities function", () => {
     let context;
     let callback = jest.fn();
 
-    beforeEach(async () => {
+    beforeAll(async () => {
         client = await getMongoClient();
-        await client.createCollection(ACTIVITIES_COLLECTION);
-        await client.createCollection(ATHLETES_COLLECTION);
+        await client.db(DB_NAME).collection(ACTIVITIES_COLLECTION).deleteMany({});
+        await client.db(DB_NAME).collection(ATHLETES_COLLECTION).deleteMany({});
 
-        await client.collection(ATHLETES_COLLECTION).insertOne({
-            id: mockedActivity.athlete.id,
-            expires_at: moment.utc().unix() + 1000,
-            access_token: "access_token",
-            refresh_token: "refresh_token",
-            clubs: [
-                {
-                    id: 1,
-                    name: "clubaname1"
-                },
-                {
-                    id: 2,
-                    name: "clubaname2"
-                }
-            ]
-        });
+        await client
+            .db(DB_NAME)
+            .collection(ATHLETES_COLLECTION)
+            .insertOne({
+                id: mockedActivity.athlete.id,
+                expires_at: moment.utc().unix() + 1000,
+                access_token: "access_token",
+                refresh_token: "refresh_token",
+                clubs: [
+                    {
+                        id: 1,
+                        name: "clubaname1",
+                    },
+                    {
+                        id: 2,
+                        name: "clubaname2",
+                    },
+                ],
+            });
     });
 
     afterAll(async () => {
-        await client.dropCollection(ACTIVITIES_COLLECTION);
-        await client.dropCollection(ATHLETES_COLLECTION);
+        client.close();
     });
 
     beforeEach(() => {
         context = {
             succeed: jest.fn(),
-            fail: jest.fn()
+            fail: jest.fn(),
         };
         callback = jest.fn();
     });
@@ -64,9 +66,9 @@ describe("Cycle2work activities function", () => {
             {
                 httpMethod: "GET",
                 queryStringParameters: {
-                    "hub.challenge": "hub.challenge"
+                    "hub.challenge": "hub.challenge",
                 },
-                body: null
+                body: null,
             },
             context,
             callback
@@ -76,8 +78,8 @@ describe("Cycle2work activities function", () => {
         expect(callback).toHaveBeenCalledWith(null, {
             statusCode: 200,
             body: JSON.stringify({
-                "hub.challenge": "hub.challenge"
-            })
+                "hub.challenge": "hub.challenge",
+            }),
         });
     });
 
@@ -85,7 +87,7 @@ describe("Cycle2work activities function", () => {
         await handler(
             {
                 httpMethod: "POST",
-                body: `{"object_id": ${mockedActivity.id}, "owner_id": ${mockedActivity.athlete.id}}`
+                body: `{"object_id": ${mockedActivity.id}, "owner_id": ${mockedActivity.athlete.id}}`,
             },
             context,
             callback
@@ -94,7 +96,7 @@ describe("Cycle2work activities function", () => {
         await handler(
             {
                 httpMethod: "POST",
-                body: `{"object_id": ${mockedActivity.id}, "owner_id": ${mockedActivity.athlete.id}}`
+                body: `{"object_id": ${mockedActivity.id}, "owner_id": ${mockedActivity.athlete.id}}`,
             },
             context,
             callback
@@ -102,20 +104,17 @@ describe("Cycle2work activities function", () => {
 
         expect(context.succeed).toHaveBeenCalledTimes(2);
 
-        const activities = await client
-            .collection(ACTIVITIES_COLLECTION)
-            .find({})
-            .toArray();
+        const activities = await client.db(DB_NAME).collection(ACTIVITIES_COLLECTION).find({}).toArray();
 
         expect(activities.length).toBe(2);
 
-        expect(activities.find(x => x.club.id === 1)).toBeDefined();
-        expect(activities.find(x => x.club.id === 2)).toBeDefined();
+        expect(activities.find((x) => x.club.id === 1)).toBeDefined();
+        expect(activities.find((x) => x.club.id === 2)).toBeDefined();
 
         expect(strava.refreshToken).toHaveBeenCalledTimes(0);
         expect(strava.getActivity).toHaveBeenCalledWith({ access_token: "access_token", id: mockedActivity.id });
 
-        activities.forEach(x => {
+        activities.forEach((x) => {
             expect(x.name).toBe("#cycle2work yo!");
             expect(x.athlete.id).toBe(134815);
             expect(x.month).toBe(2);
@@ -124,23 +123,26 @@ describe("Cycle2work activities function", () => {
     });
 
     it("refresh token if expired", async () => {
-        await client.collection(ATHLETES_COLLECTION).updateOne(
-            { id: mockedActivity.athlete.id },
-            {
-                $set: { expires_at: 0 }
-            }
-        );
+        await client
+            .db(DB_NAME)
+            .collection(ATHLETES_COLLECTION)
+            .updateOne(
+                { id: mockedActivity.athlete.id },
+                {
+                    $set: { expires_at: 0 },
+                }
+            );
 
         await handler(
             {
                 httpMethod: "POST",
-                body: `{"object_id": ${mockedActivity.id}, "owner_id": ${mockedActivity.athlete.id}}`
+                body: `{"object_id": ${mockedActivity.id}, "owner_id": ${mockedActivity.athlete.id}}`,
             },
             context,
             callback
         );
 
-        const athlete = await client.collection(ATHLETES_COLLECTION).findOne({ id: mockedActivity.athlete.id });
+        const athlete = await client.db(DB_NAME).collection(ATHLETES_COLLECTION).findOne({ id: mockedActivity.athlete.id });
 
         expect(context.succeed).toHaveBeenCalledTimes(1);
         expect(strava.refreshToken).toHaveBeenCalledWith("refresh_token");
