5c330aa2c03f88ac5e2d01a54de8551024124d41
Manuel Serra <nitroin@hotmail.com>
2020-07-16 17:28:00 +0200

    Not cron (#3)
    feat: add and save an activity without waiting for cron task

--- src/services/mongo-db.js
+++ src/services/mongo-db.js
@@ -1,27 +1,39 @@
-import { MongoClient } from "mongodb";
+import mongodb from "mongodb";
 
-import { MONGODB_URL, ACTIVITIES_COLLECTION, ATHLETES_COLLECTION, DB_NAME } from "../config";
+import { MONGODB_URL, ACTIVITIES_COLLECTION, ATHLETES_COLLECTION, DB_NAME, PROCESSED_ACTIVITIES_COLLECTION } from "../config";
 
 let clientInstance;
 
 export async function getMongoClient() {
     if (!clientInstance) {
-        clientInstance = await MongoClient.connect(MONGODB_URL, { useUnifiedTopology: true });
+        /* eslint-disable-next-line */
+        clientInstance = await mongodb.connect(MONGODB_URL, {
+            useNewUrlParser: true,
+            useUnifiedTopology: true,
+        });
     }
-    return clientInstance.db(DB_NAME);
+    return clientInstance;
 }
 
 export async function retrieveAthlete(athleteId) {
     const client = await getMongoClient();
-    return await client.collection(ATHLETES_COLLECTION).findOne({ id: athleteId });
+    return await client.db(DB_NAME).collection(ATHLETES_COLLECTION).findOne({ id: athleteId });
 }
 
 export async function updateAthleteToken(athleteId, access_token, refresh_token, expires_at) {
     const client = await getMongoClient();
-    await client.collection(ATHLETES_COLLECTION).updateOne({ id: athleteId }, { $set: { access_token, refresh_token, expires_at } });
+    await client
+        .db(DB_NAME)
+        .collection(ATHLETES_COLLECTION)
+        .updateOne({ id: athleteId }, { $set: { access_token, refresh_token, expires_at } });
 }
 
 export async function upsertActivity(activity) {
     const client = await getMongoClient();
-    await client.collection(ACTIVITIES_COLLECTION).replaceOne({ _id: activity._id }, activity, { upsert: true });
+    await client.db(DB_NAME).collection(ACTIVITIES_COLLECTION).replaceOne({ _id: activity._id }, activity, { upsert: true });
+}
+
+export async function upsertProcessedActivity(activity) {
+    const client = await getMongoClient();
+    await client.db(DB_NAME).collection(PROCESSED_ACTIVITIES_COLLECTION).replaceOne({ _id: activity._id }, activity, { upsert: true });
 }
